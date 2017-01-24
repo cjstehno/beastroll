14c81a92018272384ccea82e03db811d389d6d0b
Christopher J. Stehno <cjstehno@users.noreply.github.com>
2017-01-24 13:44:17 -0600

    Added a couple more beasts.

--- src/main/groovy/com/stehno/beast/Beasts.groovy
+++ src/main/groovy/com/stehno/beast/Beasts.groovy
@@ -9,6 +9,10 @@ class Beasts {
             attack 'ram', 'd20+5', 'd6+3'
             attack 'hooves', 'd20+5', '2d4+3'
         }
+        beast('giant-elk') {
+            attack 'ram', 'd20+6', '2d6+4'
+            attack 'hooves', 'd20+6', '4d8+4'
+        }        
         beast('giant-badger') {
             attack 'bite', 'd20+3', 'd6+1'
             attack 'claw', 'd20+3', '2d4+1'
@@ -17,6 +21,14 @@ class Beasts {
             attack 'gore', 'd20+8', '3d8+5'
             attack 'stomp', 'd20+8', '3d10+5'
         }
+        beast('black-bear'){
+            attack 'bite', 'd20+3', 'd6+2'
+            attack 'claw', 'd20+3', '2d4+2'
+        }        
+        beast('brown-bear'){
+            attack 'bite', 'd20+5', 'd8+4'
+            attack 'claw', 'd20+5', '2d6+4'
+        }         
     }
 
     void beast(String name, @DelegatesTo(Beast) Closure closure) {
